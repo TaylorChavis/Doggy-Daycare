0ebfe3f63534cb5dbd4521e818c4313c8772c5a1
Taylor <taylor.chavis62@gmail.com>
2025-11-21 16:07:10 -0500

    built src folders and added user/auth features

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 
 import userRoutes from './routes/userRoutes.js';
+import authRoutes from './src/routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ app.use(morgan('tiny'));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/auth', authRoutes);  
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
